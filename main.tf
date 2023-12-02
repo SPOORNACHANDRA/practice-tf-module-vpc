6d5fe1e843bbcc744a79f4376980f38d24a70458
s poorna chandra <poornanaidu1@gmail.com>
2023-12-02 19:24:16 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
 }
+
+resource "aws_subnet" "main" {
+  for_each = var.subnets
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value["cidr"]
+  availability_zone = each.value["az"]
+}
