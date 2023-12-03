e2fe6caac9a251a6a1d660278ccc9ec3088b81ea
s poorna chandra <poornanaidu1@gmail.com>
2023-12-03 19:00:42 +0530

    terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,13 +1,16 @@
 resource "aws_subnet" "main" {
   for_each = var.subnets
   vpc_id = var.vpc_id
-  availability_zone = each.value["az"]
   cidr_block = each.value["cidr"]
+  availability_zone = each.value["az"]
   tags = {
     name = each.key
   }
 }
 
 
-variable "vpc_id" {}
+
 variable "subnets" {}
+variable "vpc_id" {}
+
+
