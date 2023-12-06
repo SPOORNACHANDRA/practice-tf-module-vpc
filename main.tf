e52e833b0c21d36e8d8382939a24610f7ab615ca
s poorna chandra <poornanaidu1@gmail.com>
2023-12-06 19:10:37 +0530

    terraform

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_route" "ngw" {
   count = local.private_route_table_ids
   route_table_id         = element(local.private_route_table_ids,count.index )
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = element(aws_eip.ngw.*.id,count.index)
+  nat_gateway_id             = element(aws_eip.ngw.*.id,count.index)
 }
 
 
