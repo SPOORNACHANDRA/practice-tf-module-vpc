995106032dbc7e9ee3f1de95923d29d53a221858
s poorna chandra <poornanaidu1@gmail.com>
2023-12-06 17:42:18 +0530

    terraform

--- main.tf
+++ main.tf
@@ -20,11 +20,3 @@ resource "aws_internet_gateway" "igw" {
 #output "subnet" {
 #  value = module.subnets
 #}
-
-resource "aws_route_table" "main" {
-  for_each = var.subnets
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = each.key
-  }
-}
