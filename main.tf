db6a1d5e02177dac0478baac818569fdce46e8f6
s poorna chandra <poornanaidu1@gmail.com>
2023-12-08 09:57:08 +0530

    terraform

--- main.tf
+++ main.tf
@@ -17,22 +17,68 @@ resource "aws_internet_gateway" "igw" {
     Name = "main"
   }
 }
-output "subnet" {
-  value = module.subnets
-}
+
 resource "aws_route" "igw" {
   for_each               = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
   route_table_id         = each.value["id"]
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
-#
-#resource "aws_route_table" "example" {
-#  vpc_id = aws_vpc.example.id
-#
-#  route {
-#    cidr_block = "10.0.1.0/24"
-#    gateway_id = aws_internet_gateway.example.id
-#  }
+
+output "subnet" {
+  value = module.subnets
+}
+
+resource "aws_eip" "ngw" {
+  count = length(local.public_subnet_ids)
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  count = length(local.public_subnet_ids)
+  allocation_id = element(aws_eip.ngw.*.id,count.index)
+  subnet_id     = element(local.public_subnet_ids,count.index)
+}
 
 
+
+#resource "aws_eip" "ngw" {
+#  count = length(local.public_subnet_ids)       # we need how many subnets are there those many eip
+#  domain   = "vpc"
+#}
+#
+#resource "aws_nat_gateway" "ngw" {                #how many ngw i want how many public subnets
+#  count = length(local.public_subnet_ids)            # here we can take either output or input we taken form output
+#  allocation_id = element(aws_eip.ngw.*.id,count.index)
+#  subnet_id     = element(local.public_subnet_ids,count.index )
+#}
+#
+
+#resource "aws_route" "ngw" {
+#count = length(local.private_route_table_ids)
+#route_table_id         = element(local.private_route_table_ids,count.index )
+#destination_cidr_block = "0.0.0.0/0"
+#nat_gateway_id             = element(aws_nat_gateway.ngw.*.id,count.index)
+#}
+#resource "aws_vpc_peering_connection" "peering" {
+#peer_vpc_id = aws_vpc.main.id
+#vpc_id      = var.default_vpc_id
+#auto_accept = true
+#}
+#
+#resource "aws_route" "peer" {
+#count = length(local.private_route_table_ids)
+#route_table_id         = element(local.private_route_table_ids,count.index )
+#destination_cidr_block = var.default_vpc_cidr
+#vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+#}
+#
+#resource "aws_route" "default-peer-entry" {
+#route_table_id         = var.default_vpc_route_table_id
+#destination_cidr_block = var.cidr
+#vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+#tags = {
+#Name= "default peering connection"
+#}
+#}
+#
