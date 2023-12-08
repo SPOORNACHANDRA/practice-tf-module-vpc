1b93b3a2ecae9f47c7a0ce87b0ea443eb5c6fd08
s poorna chandra <poornanaidu1@gmail.com>
2023-12-08 08:22:10 +0530

    terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -18,19 +18,16 @@ resource "aws_route_table" "main" {
 
   }
 }
+resource "aws_route_table_association" "a" {
+  for_each = var.subnets
+  subnet_id      = lookup(lookup(aws_subnet.main,each.key,null ),"id",null)
+  route_table_id = lookup(lookup(aws_route_table.main,each.key,null),"id",null)
+}
 
 
 
 
 
-#resource "aws_route_table" "main" {
-#  for_each = var.subnets
-#  vpc_id = var.vpc_id
-#  tags = {
-#    Name = each.key
-#  }
-#}
-#
 #resource "aws_route_table_association" "a" {
 #  for_each = var.subnets
 #  subnet_id      = lookup(lookup(aws_subnet.main,each.key, null),"id", null)
