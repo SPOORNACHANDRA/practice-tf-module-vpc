a4f11d2f508c43d803abc7066adf2ec6d1eeba5c
s poorna chandra <poornanaidu1@gmail.com>
2023-12-08 08:54:03 +0530

    terraform

--- main.tf
+++ main.tf
@@ -24,5 +24,15 @@ resource "aws_route" "igw" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
+#
+#resource "aws_route_table" "example" {
+#  vpc_id = aws_vpc.example.id
+#
+#  route {
+#    cidr_block = "10.0.1.0/24"
+#    gateway_id = aws_internet_gateway.example.id
+#  }
 
-
+output "subnet" {
+  value = module.subnets
+}
