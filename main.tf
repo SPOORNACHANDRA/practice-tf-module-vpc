56751c1d9d697e410834c74a464155a3cde29024
s poorna chandra <poornanaidu1@gmail.com>
2023-12-08 07:57:38 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 resource "aws_vpc" "main" {         #we are consuming the variables here
   cidr_block = var.cidr
 }
+
+
+module "subnets" {
+  source = "./subnets"
+  for_each = var.subnets
+  subnets = each.value
+  vpc_id  = aws_vpc.main.id
+}
--- vars.tf
+++ vars.tf
@@ -1,1 +1,2 @@
 variable "cidr" {}  #this is a varabile recieving from the main.tf roboshop-terraform-v1
+variable "subnets" {}
