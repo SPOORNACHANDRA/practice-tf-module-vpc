ceec9b5e6bd8bff74ae1a0d3dc50a54eedb69ddb
s poorna chandra <poornanaidu1@gmail.com>
2023-12-08 08:15:58 +0530

    terraform

--- main.tf
+++ main.tf
@@ -12,12 +12,3 @@ module "subnets" {
 
 
 
-
-
-#module "subnets" {            # we have subnets in that subnets in that subnet we have multiple subnets in that subnets we have multiple values
-##we have so that we created another module called subnets so we are sending the information
-#for_each = var.subnets      # how many subnets we need so that we create a for_each so that it iterates those many times
-#source = "./subnets"
-#subnets = each.value        # this we created becoz we have multiple subnets in that subnets we have multiple sub-subnets  we are sending the info
-#vpc_id = aws_vpc.main.id
-#}
