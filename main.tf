c0d8000ce579896dae467cc712203f0436210271
s poorna chandra <poornanaidu1@gmail.com>
2023-12-08 11:06:42 +0530

    terraform

--- main.tf
+++ main.tf
@@ -68,8 +68,5 @@ resource "aws_route" "default-peer-entry" {
   route_table_id         = var.default_vpc_route_table_id
   destination_cidr_block = var.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
-  tags = {
-    Name= "default peering connection"
-  }
 }
 
