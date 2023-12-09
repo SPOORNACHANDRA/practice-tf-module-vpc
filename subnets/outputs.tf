ab08fe5253ef063099ac256b07053695e81958b3
s poorna chandra <poornanaidu1@gmail.com>
2023-12-09 17:54:05 +0530

    terraform

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,12 +1,12 @@
-#output "subnet_ids" {        #for attaching igw to public subnets only
-#  value = aws_subnet.main     # first of all these guy has to send out the info to the tf-module-vpc main.tf
-#}
-#
-#
-#output "route_table_ids" {      #for attaching igw to public subnets only
-#  value = aws_route_table.main   # first of all these guy has to send out the info to the tf-module-vpc main.tf
-#}
-#
-#output "vpc_id" {
-#  value = aws_vpc.main.id
-#}
+output "subnet_ids" {        #for attaching igw to public subnets only
+  value = aws_subnet.main     # first of all these guy has to send out the info to the tf-module-vpc main.tf
+}
+
+
+output "route_table_ids" {      #for attaching igw to public subnets only
+  value = aws_route_table.main   # first of all these guy has to send out the info to the tf-module-vpc main.tf
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
